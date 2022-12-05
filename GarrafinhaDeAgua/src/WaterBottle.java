e34de07f97d7fdfb6e2a5656ec524c1b07206bb4
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2022-12-05 17:26:01 -0400

    Created the Interface methods and the attributes

--- GarrafinhaDeAgua/src/WaterBottle.java
+++ GarrafinhaDeAgua/src/WaterBottle.java
@@ -1,5 +1,11 @@
 public class WaterBottle implements Bottle{
 
+    //variables
+    private String bottleType;
+    private boolean tampada;
+    private boolean empty;
+    private float howMuchFilled;
+
     @Override
     public void cap() {
         // TODO Auto-generated method stub
